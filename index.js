ecb6887ed465a91119d35588532747babfc653c6
Erik Baklund <erik@hubii.com>
2019-03-08 11:41:28 +0100

    Export EthereumHash and NahmiiBinary

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const EthereumAddress = require('./lib/ethereum-address');
+const EthereumHash = require('./lib/ethereum-hash');
+const NahmiiBinary = require('./lib/nahmii-binary');
 
 module.exports = {
-    EthereumAddress
+    EthereumAddress,
+    EthereumHash,
+    NahmiiBinary
 };
